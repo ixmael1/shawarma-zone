c0445ec6eb5e9e5dbeada113f5fae64ede94fbf2
Al-Amin Bashir Babuga <113430900+babuga01@users.noreply.github.com>
2025-05-26 12:35:30 +0100

    editing homepage

--- public/header.php
+++ public/header.php
@@ -5,7 +5,8 @@ $path = basename($_SERVER['PHP_SELF']);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Shawarma Zone</title>
   <link rel="stylesheet" href="/public/css/main.css">
   <!-- Font-Awesome for icons -->
